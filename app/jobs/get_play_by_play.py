dee7fbda8b76a8937a7c1db872df76f703ab126e
shind <shind@sfu.ca>
2020-12-15 13:31:41 -0800

    run jobs with database load

--- app/jobs/get_play_by_play.py
+++ app/jobs/get_play_by_play.py
@@ -3,26 +3,22 @@ https://gist.github.com/Deryck97/dff8d33e9f841568201a2a0d5519ac5e
 https://github.com/guga31bb/nflfastR-data/tree/master/data
 """
 import logging
-import os
 
 import pandas as pd
 
 from app.config import (
     configure_logging,
     CURRENT_YEAR,
-    PLAY_BY_PLAY_DIRECTORY
 )
-from app.utils import (
-    init_directory,
-)
+from app.db import get_db_conn
 
 REMOTE_PATH_TEMPLATE = 'https://github.com/guga31bb/nflfastR-data/blob/master/data/play_by_play_{year}.csv.gz?raw=True'
+OUTPUT_TABLE_NAME = 'play_by_play'
 
 
 def _extract(path: str) -> pd.DataFrame:
     """Download CSV from remote path."""
     logging.info(f"Downloading remote play by play CSV from {path}...")
-
     return pd.read_csv(
         path,
         compression='gzip' if "csv.gz" in path else "infer",
@@ -30,25 +26,22 @@ def _extract(path: str) -> pd.DataFrame:
     )
 
 
-def _load(df: pd.DataFrame, path: str) -> None:
-    """Write DF to local CSV."""
-    init_directory(PLAY_BY_PLAY_DIRECTORY)
-    logging.info(f"Writing play by play CSV to {path}...")
-
-    df.to_csv(path, index=False)
+def _load(db_conn, df: pd.DataFrame) -> None:
+    """Write DF to database."""
+    logging.info(f"Writing {len(df)} rows to {OUTPUT_TABLE_NAME}...")
+    df.to_sql(OUTPUT_TABLE_NAME, db_conn, index=False, if_exists='replace')
 
 
-def run():
+def run(year: str = CURRENT_YEAR) -> None:
     logging.info("Getting play by play data...")
-
-    remote_path = REMOTE_PATH_TEMPLATE.format(year=CURRENT_YEAR)
+    remote_path = REMOTE_PATH_TEMPLATE.format(year=year)
     df = _extract(remote_path)
 
-    local_path = os.path.join(PLAY_BY_PLAY_DIRECTORY, 'play_by_play.csv')
-    _load(df, local_path)
+    db_conn = get_db_conn()
+    _load(db_conn, df)
+    logging.info("Play by play data loaded to db.")
 
 
 if __name__ == "__main__":
     configure_logging()
-
     run()
--- app/jobs/get_roster.py
+++ app/jobs/get_roster.py
@@ -1,27 +1,17 @@
-"""
-https://gist.github.com/Deryck97/dff8d33e9f841568201a2a0d5519ac5e
-https://github.com/guga31bb/nflfastR-data/tree/master/data
-"""
 import logging
-import os
 
 import pandas as pd
 
-from app.config import (
-    configure_logging,
-    ROSTER_DIRECTORY
-)
-from app.utils import (
-    init_directory,
-)
+from app.config import configure_logging
+from app.db import get_db_conn
 
 REMOTE_PATH = 'https://github.com/mrcaseb/nflfastR-roster/blob/master/data/nflfastR-roster.csv.gz?raw=True'
+OUTPUT_TABLE_NAME = 'roster'
 
 
 def _extract(path: str) -> pd.DataFrame:
     """Download CSV from remote path."""
     logging.info(f"Downloading remote roster CSV from {path}...")
-
     return pd.read_csv(
         path,
         compression='gzip' if "csv.gz" in path else "infer",
@@ -29,24 +19,21 @@ def _extract(path: str) -> pd.DataFrame:
     )
 
 
-def _load(df: pd.DataFrame, path: str) -> None:
-    """Write DF to local CSV."""
-    init_directory(ROSTER_DIRECTORY)
-    logging.info(f"Writing roster CSV to {path}...")
-
-    df.to_csv(path, index=False)
+def _load(db_conn, df: pd.DataFrame) -> None:
+    """Write DF to database."""
+    logging.info(f"Writing {len(df)} rows to {OUTPUT_TABLE_NAME}...")
+    df.to_sql(OUTPUT_TABLE_NAME, db_conn, index=False, if_exists='replace')
 
 
-def run():
+def run() -> None:
     logging.info("Getting roster data...")
-
     df = _extract(REMOTE_PATH)
 
-    local_path = os.path.join(ROSTER_DIRECTORY, 'roster.csv')
-    _load(df, local_path)
+    db_conn = get_db_conn()
+    _load(db_conn, df)
+    logging.info("Roster data loaded to db.")
 
 
 if __name__ == "__main__":
     configure_logging()
-
     run()
