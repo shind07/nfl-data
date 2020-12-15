8ec7d95f5b491e349ca0630caf6e473c69dc6983
shind <shind@sfu.ca>
2020-12-15 09:42:02 -0800

    Create rushing tables

---
+++ alembic/versions/c0cea8505ac5_create_rushing_tables.py
@@ -0,0 +1,43 @@
+"""create rushing tables
+
+Revision ID: c0cea8505ac5
+Revises: c2019f5f4de1
+Create Date: 2020-12-15 05:52:47.386708
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c0cea8505ac5'
+down_revision = 'c2019f5f4de1'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute("""
+        CREATE TABLE rushing_by_player_by_game (
+            game_id VARCHAR NOT NULL,
+            team VARCHAR NOT NULL,
+            def_team VARCHAR NOT NULL,
+            week DECIMAL NOT NULL,
+            rusher_id VARCHAR NOT NULL,
+            rusher VARCHAR NOT NULL,
+            rush_type VARCHAR NOT NULL,
+            attempts INTEGER NOT NULL,
+            yards INTEGER NOT NULL,
+            td INTEGER NOT NULL,
+            fumbles INTEGER NOT NULL,
+            fumbles_lost INTEGER NOT NULL,
+            fumbles_out_of_bounds INTEGER NOT NULL,
+            epa INTEGER NOT NULL
+        );
+    """)
+
+
+def downgrade():
+    op.execute("""
+        DROP TABLE IF EXISTS rushing_by_player_by_game;
+    """)
