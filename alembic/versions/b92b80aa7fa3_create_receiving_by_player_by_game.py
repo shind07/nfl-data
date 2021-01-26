5cc2f91bdb0d8eb3c88bd48b339b284338b23674
shind <shind@sfu.ca>
2021-01-26 09:11:36 -0800

    create receiving jobs and tables

---
+++ alembic/versions/b92b80aa7fa3_create_receiving_by_player_by_game.py
@@ -0,0 +1,42 @@
+"""create receiving by player by game
+
+Revision ID: b92b80aa7fa3
+Revises: ba3fad0c46be
+Create Date: 2021-01-26 08:42:50.486437
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b92b80aa7fa3'
+down_revision = 'ba3fad0c46be'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute("""
+        CREATE TABLE receiving_by_player_by_game (
+            year SMALLINT NOT NULL,
+            season_type VARCHAR NOT NULL,
+            team VARCHAR NOT NULL,
+            receiver VARCHAR NOT NULL,
+            week SMALLINT NOT NULL,
+            opp VARCHAR NOT NULL,
+            receptions SMALLINT NOT NULL,
+            targets SMALLINT NOT NULL,
+            yards SMALLINT NOT NULL,
+            air_yards_intended SMALLINT NOT NULL,
+            air_yards_completed SMALLINT NOT NULL,
+            td SMALLINT NOT NULL,
+            int SMALLINT NOT NULL,
+            fumbles SMALLINT NOT NULL,
+            epa DECIMAL NOT NULL
+        );
+    """)
+
+
+def downgrade():
+    op.execute("""DROP TABLE IF EXISTS receiving_by_player_by_game""")
---
+++ alembic/versions/d670a0e93945_create_receiving_by_player_by_year.py
@@ -0,0 +1,42 @@
+"""create receiving by player by year
+
+Revision ID: d670a0e93945
+Revises: b92b80aa7fa3
+Create Date: 2021-01-26 08:50:19.380589
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd670a0e93945'
+down_revision = 'b92b80aa7fa3'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute("""
+        CREATE TABLE receiving_by_player_by_year (
+            year DECIMAL NOT NULL,
+            season_type VARCHAR NOT NULL,
+            team VARCHAR NOT NULL,
+            receiver VARCHAR,
+            position VARCHAR,
+            games SMALLINT NOT NULL,
+            receptions SMALLINT NOT NULL,
+            targets SMALLINT NOT NULL,
+            yards SMALLINT NOT NULL,
+            air_yards_intended SMALLINT NOT NULL,
+            air_yards_completed SMALLINT NOT NULL,
+            td SMALLINT NOT NULL,
+            int SMALLINT NOT NULL,
+            fumbles SMALLINT NOT NULL,
+            epa DECIMAL NOT NULL
+        );
+    """)
+
+
+def downgrade():
+    op.execute("""DROP TABLE IF EXISTS receiving_by_player_by_year""")
