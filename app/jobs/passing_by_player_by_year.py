cd502e0f976cd92ea949ff4ff290e23081875ea2
shind <shind@sfu.ca>
2021-02-20 18:45:49 -0800

    refactor passing by player by year

--- app/jobs/passing_by_player_by_year.py
+++ app/jobs/passing_by_player_by_year.py
@@ -3,10 +3,12 @@ Upstream jobs: play_by_play_enriched
 """
 import logging
 
+import numpy as np
 import pandas as pd
 
 from app.config import (
     configure_logging,
+    YEAR_GROUPING_COLUMNS
 )
 from app.db import get_db_eng, load
 
@@ -17,69 +19,41 @@ OUTPUT_TABLE_NAME = "passing_by_player_by_year"
 def _extract(db_conn) -> pd.DataFrame:
     """Getting passing stats, per player per year"""
     logging.info("Extracting passing stats by player by game from play by play...")
-    query = """
-        SELECT
-            year,
-            season_type,
-            posteam AS team,
-            passer_gsis_id AS gsis_id,
-            passer_position AS pos,
-            passer as player,
-            COUNT(DISTINCT p.game_id) AS games,
-            SUM(complete_pass) AS completions,
-            SUM(pass_attempt) AS attempts,
-            SUM(CASE
-                WHEN lateral_rec_yards IS NOT NULL AND sack = 0 THEN yards_gained + lateral_rec_yards
-                WHEN sack = 0 THEN yards_gained
-                ELSE 0 END
-            ) AS yards,
-            SUM(CASE WHEN sack = 0 THEN air_yards ELSE 0 END) AS air_yards_intended,
-            SUM(CASE WHEN complete_pass = 1 THEN air_yards ELSE 0 END) AS air_yards_completed,
-            SUM(pass_touchdown) AS td,
-            SUM(interception) AS int,
-            SUM(sack) as sacks,
-            SUM(CASE WHEN sack = 1 THEN yards_gained ELSE 0 END) AS sack_yards,
-            SUM(fumble) AS fumbles,
-            SUM(fumble_lost) AS fumbles_lost,
-            SUM(CASE WHEN play_type = 'qb_spike' THEN 1 ELSE 0 END) AS spikes,
-            SUM(CASE WHEN play_type != 'qb_spike' THEN epa ELSE 0 END) AS pass_epa,
-            SUM(epa) AS epa,
-            SUM(CASE WHEN play_type = 'qb_spike' THEN epa ELSE 0 END) AS spike_epa,
-            CASE WHEN
-                SUM(cpoe) IS NULL OR SUM(pass_attempt) = 0 THEN 0
-                ELSE SUM(cpoe) / SUM(pass_attempt) END
-            AS cpoe
-        FROM
-            play_by_play_enriched AS p
-        LEFT JOIN
-            (SELECT
-                game_id,
-                play_id,
-                SUM(lateral_rec_yards) as lateral_rec_yards
-            FROM
-                lateral_receiving_yards
-            GROUP BY
-                game_id, play_id
-            ) AS l
-        ON
-            p.game_id = l.game_id AND p.play_id = l.play_id
-        WHERE
-            (play_type = 'pass' or play_type = 'qb_spike')
-            AND two_point_attempt = 0
-        GROUP BY
-            year, posteam, passer_gsis_id, p.passer_position, passer, season_type
-        ORDER BY
-            epa DESC
-    """
+    query = """SELECT * FROM passing_by_player_by_game"""
     df = pd.read_sql(query, db_conn)
     logging.info(f"Extracted {len(df)} rows of passing stats.")
     return df
+
+
+def _transform(df: pd.DataFrame) -> pd.DataFrame:
+    """Aggregate per game stats to the year level"""
+    logging.info("Aggregating per game passing stats to per year...")
+    df = df[[col for col in df.columns if "_share" not in col]]
+    df = df.groupby(YEAR_GROUPING_COLUMNS, as_index=False).sum()
+    df = df.drop('week', axis=1)
+
+    df['target_share_wr'] = df['attempts_wr'] / df['attempts']
+    df['target_share_te'] = df['attempts_te'] / df['attempts']
+    df['target_share_rb'] = df['attempts_rb'] / df['attempts']
+    df['target_share_other'] = df['attempts_other'] / df['attempts']
+
+    df['air_yards_intended_share_wr'] = df['air_yards_intended_wr'] / df['air_yards_intended']
+    df['air_yards_intended_share_te'] = df['air_yards_intended_te'] / df['air_yards_intended']
+    df['air_yards_intended_share_rb'] = df['air_yards_intended_rb'] / df['air_yards_intended']
+
+    df['air_yards_completed_share_wr'] = df['air_yards_completed_wr'] / df['air_yards_completed']
+    df['air_yards_completed_share_te'] = df['air_yards_completed_te'] / df['air_yards_completed']
+    df['air_yards_completed_share_rb'] = df['air_yards_completed_rb'] / df['air_yards_completed']  
+    
+    df = df.replace([np.inf, -np.inf], np.nan)
+    return df.fillna(0) 
 
 
 def run() -> None:
     logging.info(f"Running job for {OUTPUT_TABLE_NAME}...")
     with get_db_eng().connect() as db_conn:
         df = _extract(db_conn)
+        df = _transform(df)
         load(db_conn, df, OUTPUT_TABLE_NAME)
         logging.info(f"Job for {OUTPUT_TABLE_NAME} complete.")
 
