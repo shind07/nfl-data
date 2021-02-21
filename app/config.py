ee3f25bf0be133d15bb0a9e08bf0537e4ce9f651
shind <shind@sfu.ca>
2021-02-21 09:14:47 -0800

    share grouping variables

--- app/config.py
+++ app/config.py
@@ -15,8 +15,10 @@ HEADSHOTS_DIRECTORY = os.path.join(DATA_DIRECTORY, 'headshots')
 TEAM_LOGOS_DIRECTORY = os.path.join(DATA_DIRECTORY, 'logos')
 TEAM_LOGOS_CSV_PATH = os.path.join(TEAM_LOGOS_DIRECTORY, 'teams_logos_colors.csv')
 
-GAME_GROUPING_COLUMNS = ['year', 'season_type', 'game_id', 'team', 'opp', 'week', 'gsis_id', 'pos', 'player']
-YEAR_GROUPING_COLUMNS = ['year', 'season_type', 'team', 'gsis_id', 'pos', 'player']
+PLAYER_GAME_GROUPING_COLUMNS = ['year', 'season_type', 'game_id', 'team', 'opp', 'week', 'gsis_id', 'pos', 'player']
+PLAYER_YEAR_GROUPING_COLUMNS = ['year', 'season_type', 'team', 'gsis_id', 'pos', 'player']
+TEAM_GAME_GROUPING_COLUMNS = ['year', 'season_type', 'game_id', 'team', 'opp', 'week']
+TEAM_YEAR_GROUPING_COLUMNS = ['year', 'season_type', 'team']
 
 
 def configure_logging():
