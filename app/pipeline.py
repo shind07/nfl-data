3fd11fd4820249fc3b18d8b01011bd2a161b08b2
shind <shind@sfu.ca>
2021-03-06 20:03:59 -0800

    add 2 pt conversions

--- app/pipeline.py
+++ app/pipeline.py
@@ -21,22 +21,24 @@ from app.jobs import (
     rushing_by_player_by_year,
     rushing_by_team_by_game,
     rushing_by_team_by_year,
+    two_point_conversions_by_player_by_game,
+    two_point_conversions_by_player_by_year
 )
 
 
 def run(year: int = CURRENT_YEAR):
     """Run the pipeline if there are new games in the play by play data."""
     # raw data
-    try:
-        play_by_play.run(year)
-    except play_by_play.NoNewGamesException as e:
-        logging.info(e)
-        return
+    # try:
+    #     play_by_play.run(year)
+    # except play_by_play.NoNewGamesException as e:
+    #     logging.info(e)
+    #     return
 
-    roster.run(year)
-    headshots.run()
-    lateral_receiving_yards.run(year)
-    play_by_play_enriched.run()
+    # roster.run(year)
+    # # headshots.run()
+    # lateral_receiving_yards.run(year)
+     # play_by_play_enriched.run()
 
     # aggregations
     passing_by_player_by_game.run()
@@ -51,6 +53,9 @@ def run(year: int = CURRENT_YEAR):
     rushing_by_player_by_year.run()
     rushing_by_team_by_game.run()
     rushing_by_team_by_year.run()
+
+    two_point_conversions_by_player_by_game.run()
+    two_point_conversions_by_player_by_game.run()
 
     # aggregations of aggregations
 
